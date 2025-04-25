cae483550377cd53c995540bfdce11e3a1915393
yutizf <yutizf@hotmail.com>
2025-04-25 14:07:05 -0300

    Update PersonaController.java

--- src/main/java/com/fernandoyutiz/mysql/controllers/PersonaController.java
+++ src/main/java/com/fernandoyutiz/mysql/controllers/PersonaController.java
@@ -18,7 +18,7 @@ public class PersonaController {
         this.personaService = personaService;
     }
 
-    @PostMapping("/guardapersona")
+    @PostMapping("/guardapersona1")
     public ResponseEntity<Persona> guardaPersona(@RequestBody Persona persona){
         return new ResponseEntity<>(personaService.guardarPersona(persona), HttpStatus.CREATED);
     }
